0467dbf8eaaea732c7b30edafae0373892112ecb
Mustafa Yumurtacı <mustafa.yumurtaci@hotmail.com>
2018-11-01 23:09:14 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import { BrowserRouter, Route } from "react-router-dom";
+import PostsIndex from "./components/posts-index";
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector(".container")
+);
